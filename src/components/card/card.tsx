3d1a71a13df1bd9aaaf4a770e6c771cb4eff39e4
Sam <qureshisamin25@gmail.com>
2023-12-22 02:35:23 -0500

    Init

---
+++ src/components/card/card.tsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+function Card() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default Card
